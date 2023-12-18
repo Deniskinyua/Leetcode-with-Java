5db49b08af57177c596dff4511222d8db0542f51
Denis Kinyua <kimenjudenis@gmail.com>
2023-12-18 22:47:23 +0300

    Best Time To Buy and Sell Stock: Leetcode 121

--- Day_1_OF_150/BestTimeToSellStock.java
+++ Day_1_OF_150/BestTimeToSellStock.java
@@ -28,8 +28,21 @@ Constraints:
 0 <= prices[i] <= 104
 */
 public class BestTimeToSellStock{
-
+	public int maxProfit (int [] prices){
+		
+		int minPrice = prices[0];
+		int maxProfit = 0;
+		for(int i = 1; i < prices.length; i++){
+			maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+			minPrice = Math.min(minPrice, prices[i]);
+		}
+		System.out.println("Max Profit: "+maxProfit);
+		return maxProfit;
+	}
 	public static void main(String[]args){
 
+		int [] prices = {7,1,5,3,6,4};
+		BestTimeToSellStock best = new BestTimeToSellStock();
+		best.maxProfit(prices);
 	}
 }
